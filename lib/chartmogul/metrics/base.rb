cd1eb66722dfe3248a5f7a9e24d8b3b8bdd41326
Artem Maschenko <artem.maschenko@gmail.com>
2016-06-30 14:20:35 +0300

    Review

--- lib/chartmogul/metrics/base.rb
+++ lib/chartmogul/metrics/base.rb
@@ -2,44 +2,44 @@ module ChartMogul
   module Metrics
 
     def self.all(options = {})
-      ChartMogul::Metrics::AllKeyMetricsEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::AllKeyMetricsEntries.all(preprocess_params(options))
     end
 
     def self.arpa(options = {})
-      ChartMogul::Metrics::ArpaEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::ArpaEntries.all(preprocess_params(options))
     end
 
     def self.arr(options = {})
-      ChartMogul::Metrics::ArrEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::ArrEntries.all(preprocess_params(options))
     end
 
     def self.asp(options = {})
-      ChartMogul::Metrics::AspEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::AspEntries.all(preprocess_params(options))
     end
 
     def self.customer_churn_rate(options = {})
-      ChartMogul::Metrics::CustomerChurnRateEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::CustomerChurnRateEntries.all(preprocess_params(options))
     end
 
     def self.customer_count(options = {})
-      ChartMogul::Metrics::CustomerCountEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::CustomerCountEntries.all(preprocess_params(options))
     end
 
     def self.mrr(options = {})
-      ChartMogul::Metrics::MrrEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::MrrEntries.all(preprocess_params(options))
     end
 
     def self.ltv(options = {})
-      ChartMogul::Metrics::LtvEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::LtvEntries.all(preprocess_params(options))
     end
 
     def self.mrr_churn_rate(options = {})
-      ChartMogul::Metrics::MrrChurnRateEntries.all(preprocess_list_params(options))
+      ChartMogul::Metrics::MrrChurnRateEntries.all(preprocess_params(options))
     end
 
     private
 
-    def self.preprocess_list_params(options)
+    def self.preprocess_params(options)
       [:start_date, :end_date].each do |param_name|
         options[param_name.to_s.gsub('_', '-')] = options.delete(param_name) if options[param_name]
       end
