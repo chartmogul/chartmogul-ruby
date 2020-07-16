1fa6ff56475a9813f9e3cd7b15b659e8c977e7b2
Dmitrii Davydov <swember@gmail.com>
2020-07-16 16:15:30 +0300

    Record the cassette

--- spec/chartmogul/api_resource_spec.rb
+++ spec/chartmogul/api_resource_spec.rb
@@ -3,13 +3,33 @@
 require 'spec_helper'
 
 describe ChartMogul::APIResource do
-  describe 'connection', vcr: true do
-    it 'works when credentials are updated', vcr: { record: :all } do
+  before do
+    WebMock.allow_net_connect!
+  end
+  after do
+    WebMock.disable_net_connect!
+  end
+
+  describe 'connection' do
+    it 'works when credentials are updated', vcr: { record: :all, exclusive: true } do
       set_invalid_credentials
       expect { ChartMogul::Ping.ping }.to raise_error(ChartMogul::UnauthorizedError)
 
       set_valid_credentials
-      expect { ChartMogul::Ping.ping }.not_to raise_error(ChartMogul::UnauthorizedError)
+      expect { ChartMogul::Ping.ping }.not_to raise_error
+    end
+
+    it 'works in a threaded environment', vcr: { record: :all, exclusive: true } do
+      t1 = Thread.new do
+        set_invalid_credentials
+        expect { ChartMogul::Ping.ping }.to raise_error(ChartMogul::UnauthorizedError)
+      end
+
+      t2 = Thread.new do
+        set_valid_credentials
+        expect { ChartMogul::Ping.ping }.not_to raise_error
+      end
+      [t1, t2].join
     end
   end
 
