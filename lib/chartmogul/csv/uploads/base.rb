8e9c37ab39206596219ebded6c67dc3646852379
macharmi <mohamed@chartmogul.com>
2020-09-01 08:52:36 +0200

    add tests for customer

--- lib/chartmogul/csv/uploads/base.rb
+++ lib/chartmogul/csv/uploads/base.rb
@@ -4,7 +4,7 @@ module ChartMogul
   module CSV
     module Uploads
       class Base < APIResource
-        attr_accessor :type
+        attr_reader :type, :data_source_uuid, :filename, :batch_name
 
         def initialize(data_source_uuid, filename, batch_name)
           @data_source_uuid = data_source_uuid
@@ -12,22 +12,24 @@ module ChartMogul
           @batch_name = batch_name
         end
 
+        def send
+          resp = handling_errors do
+            connection.post(resource_path.apply(data_source_uuid: @data_source_uuid)) do |req|
+              req.body = payload
+            end
+          end
+          json = ChartMogul::Utils::JSONParser.parse(resp.body)
+          ChartMogul::CSV::Uploads::Job.new_from_json(json)
+        end
+
+        private
+
         def payload
           {
             file: Faraday::FilePart.new(@filename, 'text/csv'),
             type: type,
             batch_name: @batch_name
           }
-        end
-
-        def send
-          resp = handling_errors do
-            connection.post(resource_path.apply({data_source_uuid: @data_source_uuid})) do |req|
-              req.body = payload
-            end
-          end
-          json = ChartMogul::Utils::JSONParser.parse(resp.body)
-          ChartMogul::CSV::Uploads::Job.new_from_json(json)
         end
       end
     end
