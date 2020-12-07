c73df1f862e94e3dcf61a4569ab96b98d9e2cf64
macharmi <mohamed@chartmogul.com>
2020-12-07 16:46:33 +0100

    Cancellation not working in PayPal DP [ch26991]

--- lib/chartmogul/csv/cancellation.rb
+++ lib/chartmogul/csv/cancellation.rb
@@ -7,7 +7,7 @@ module ChartMogul
       CANCELLATION_HEADERS = %w[Subscription\ external\ ID Customer\ external\ ID Date].freeze
 
       writeable_attr :subscription_external_id
-      writeable_attr :customer_external_id
+      writeable_attr :data_source_customer_external_id
       writeable_attr :cancelled_at
 
       def self.headers
