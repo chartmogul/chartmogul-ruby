434476dac4863a6871e5100c83772d9b102097eb
Maria Braganca <maria@chartmogul.com>
2021-03-03 17:28:24 +0100

    set vcr to output one single cassette

--- spec/chartmogul/account_spec.rb
+++ spec/chartmogul/account_spec.rb
@@ -3,7 +3,11 @@
 require 'spec_helper'
 
 describe ChartMogul::Account, uses_api: true do
-  describe 'API interactions', vcr: true, record: :all do
+  describe 'API interactions' do
+    around(:all) do |example|
+      VCR.use_cassette('ChartMogul_Account/returns_details_of_current_account', &example)
+    end
+
     let(:account) { ChartMogul::Account.retrieve }
 
     it 'returns the name of current account', uses_api: true do
