7e0b0a686d941988d329a0e233df29c6cb1bec65
Maria Braganca <maria@chartmogul.com>
2021-03-03 09:39:43 +0100

    updated ruby library to include the account api endpoint

---
+++ spec/chartmogul/account_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe ChartMogul::Account, uses_api: true do
+  describe 'API interactions', vcr: true, record: :all do
+    let(:account) do
+      double(
+        name: 'Example Test Company',
+        currency: 'EUR',
+        time_zone: 'Europe/Berlin',
+        week_start_on: 'sunday'
+      )
+    end
+
+    before do
+      allow(ChartMogul::Account).to receive(:retrieve).and_return(account)
+    end
+
+    it 'returns the name of current account', uses_api: true do
+      expect(account.name).to eq('Example Test Company')
+    end
+
+    it 'returns the currency of current account', uses_api: true do
+      expect(account.currency).to eq('EUR')
+    end
+
+    it 'returns the time zone of current account', uses_api: true do
+      expect(account.time_zone).to eq('Europe/Berlin')
+    end
+
+    it 'returns the week_start_on of current account', uses_api: true do
+      expect(account.week_start_on).to eq('sunday')
+    end
+  end
+end
