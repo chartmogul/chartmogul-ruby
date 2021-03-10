30592c43e42607d853f46ca7cc31ee480f66b1ff
MedAli Charmi <mohamed@chartmogul.com>
2021-03-10 15:43:27 +0100

    Add partial payments to CSV JSON processor [ch29133] (#103)

--- lib/chartmogul/csv/transaction.rb
+++ lib/chartmogul/csv/transaction.rb
@@ -4,13 +4,14 @@ module ChartMogul
   module CSV
     class Transaction < Base
       # from https://chartmogul-samples.s3-eu-west-1.amazonaws.com/public/05_Transactions.csv
-      TRANSACTION_HEADERS = %w[Invoice\ external\ ID External\ ID Type Result Date].freeze
+      TRANSACTION_HEADERS = %w[Invoice\ external\ ID External\ ID Type Result Date Amount\ in\ cents].freeze
 
       writeable_attr :invoice_external_id
       writeable_attr :external_id
       writeable_attr :type
       writeable_attr :result
       writeable_attr :transacted_at
+      writeable_attr :amount_in_cents
 
       def self.headers
         TRANSACTION_HEADERS
