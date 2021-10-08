e37f88521ea9fcfcf8e639f43dcb338a9d796ac7
Petr Kopac <pkopac@users.noreply.github.com>
2021-10-08 09:25:36 +0200

    Update invoice.rb

--- lib/chartmogul/csv/invoice.rb
+++ lib/chartmogul/csv/invoice.rb
@@ -6,7 +6,7 @@ module ChartMogul
       INVOICE_HEADERS = %w[Customer\ external\ ID Invoice\ external\ ID Invoiced\ date Due\ date Currency].freeze
 
       # from https://chartmogul-samples.s3-eu-west-1.amazonaws.com/public/03_Invoices.csv
-      writeable_attr :data_source_customer_external_id
+      writeable_attr :customer_external_id
       writeable_attr :external_id
       writeable_attr :invoiced_at
       writeable_attr :due_at
