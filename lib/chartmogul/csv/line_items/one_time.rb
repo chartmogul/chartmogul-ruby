5214485c26acb1cb9710456e4c8796a3d5dbcb12
MedAli Charmi <mohamed@chartmogul.com>
2021-11-15 10:18:58 +0100

    Add flag balance in BalanceLineItem [sc-35435] (#125)
    * Add flag balance in BalanceLineItem [sc-35435]
    
    * revert invoice status

--- lib/chartmogul/csv/line_items/one_time.rb
+++ lib/chartmogul/csv/line_items/one_time.rb
@@ -3,7 +3,7 @@
 module ChartMogul
   module CSV
     module LineItems
-      ONE_TIME_HEADERS = %w[Invoice\ external\ ID External\ ID Subscription\ external\ ID Subscription\ set\ external\ ID Type Amount\ in\ cents Plan Service\ period\ start Service\ period\ end Quantity Proration Discount\ code Discount\ amount Tax\ amount Description Transaction\ fee Account\ Code Transaction\ fees\ currency Discount\ description].freeze
+      ONE_TIME_HEADERS = %w[Invoice\ external\ ID External\ ID Subscription\ external\ ID Subscription\ set\ external\ ID Type Amount\ in\ cents Plan Service\ period\ start Service\ period\ end Quantity Proration Discount\ code Discount\ amount Tax\ amount Description Transaction\ fee Account\ Code Transaction\ fees\ currency Discount\ description Balance\ transfer].freeze
 
       class OneTime < Base
         writeable_attr :invoice_external_id
@@ -25,6 +25,7 @@ module ChartMogul
         writeable_attr :account_code
         writeable_attr :transaction_fees_currency
         writeable_attr :discount_description
+        writeable_attr :balance_transfer
 
         def type
           'one_time'
