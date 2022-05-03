bddfed0fc12bc6d8e7a37e1c723988a20044ba93
mohamed <mohamed@chartmogul.com>
2022-05-03 16:55:38 +0100

    Add event order to CSV classes in Ruby Client [sc-40413]

--- lib/chartmogul/csv/line_items/subscription.rb
+++ lib/chartmogul/csv/line_items/subscription.rb
@@ -4,7 +4,7 @@ module ChartMogul
   module CSV
     module LineItems
       # from https://chartmogul-samples.s3-eu-west-1.amazonaws.com/public/04_Invoice_line_items.csv
-      SUBSCRIPTION_HEADERS = %w[Invoice\ external\ ID External\ ID Subscription\ external\ ID Subscription\ set\ external\ ID Type Amount\ in\ cents Plan Service\ period\ start Service\ period\ end Quantity Proration Discount\ code Discount\ amount Tax\ amount Description Transaction\ fee Account\ Code Transaction\ fees\ currency Discount\ description Proration\ type].freeze
+      SUBSCRIPTION_HEADERS = %w[Invoice\ external\ ID External\ ID Subscription\ external\ ID Subscription\ set\ external\ ID Type Amount\ in\ cents Plan Service\ period\ start Service\ period\ end Quantity Proration Discount\ code Discount\ amount Tax\ amount Description Transaction\ fee Account\ Code Transaction\ fees\ currency Discount\ description Proration\ type Event\ Order].freeze
 
       class Subscription < Base
         writeable_attr :invoice_external_id
@@ -27,6 +27,7 @@ module ChartMogul
         writeable_attr :transaction_fees_currency
         writeable_attr :discount_description
         writeable_attr :proration_type
+        writeable_attr :event_order
 
         def type
           'subscription'
