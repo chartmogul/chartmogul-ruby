a0131affba5a98c90404e6b245e70314648a004c
Sebastian Cifuentes <sebastian.a.cifu@gmail.com>
2023-08-31 14:07:52 +0200

    Add event order into subscription_event.rb

--- lib/chartmogul/csv/subscription_event.rb
+++ lib/chartmogul/csv/subscription_event.rb
@@ -3,7 +3,7 @@
 module ChartMogul
   module CSV
     class SubscriptionEvent < Base
-      SUBSCRIPTION_EVENT_HEADERS = %w[External\ ID Subscription\ set\ external\ ID Subscription\ external\ ID Customer\ external\ ID Plan\ external\ ID Date Effective\ Date Event\ Type Currency Amount\ in\ Cents Quantity Retracted\ event\ ID].freeze
+      SUBSCRIPTION_EVENT_HEADERS = %w[External\ ID Subscription\ set\ external\ ID Subscription\ external\ ID Customer\ external\ ID Plan\ external\ ID Date Effective\ Date Event\ Type Currency Amount\ in\ Cents Quantity Retracted\ event\ ID Event\ Order].freeze
 
       # https://github.com/chartmogul/platform/blob/main/engines/data_ingestion/app/services/data_ingestion/csv_mapping/subscription_event.rb
       writeable_attr :external_id
@@ -18,6 +18,7 @@ module ChartMogul
       writeable_attr :amount_in_cents
       writeable_attr :quantity
       writeable_attr :retracted_event_id
+      writeable_attr :event_order
 
       def self.headers
         SUBSCRIPTION_EVENT_HEADERS
