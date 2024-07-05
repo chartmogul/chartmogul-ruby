8c1627de36cc9d8e92f239fb3fc16b098dfdb7d8
Petr Kopac <petr@chartmogul.com>
2024-07-05 14:40:24 +0200

    Fix constant duplication

--- lib/chartmogul/csv/line_items/subscription.rb
+++ lib/chartmogul/csv/line_items/subscription.rb
@@ -3,10 +3,10 @@
 module ChartMogul
   module CSV
     module LineItems
-      # from https://chartmogul-samples.s3-eu-west-1.amazonaws.com/public/04_Invoice_line_items.csv
-      SUBSCRIPTION_HEADERS = %w[Invoice\ external\ ID External\ ID Subscription\ external\ ID Subscription\ set\ external\ ID Type Amount\ in\ cents Plan Service\ period\ start Service\ period\ end Quantity Proration Discount\ code Discount\ amount Tax\ amount Description Transaction\ fee Account\ Code Transaction\ fees\ currency Discount\ description Proration\ type Event\ Order].freeze
-
       class Subscription < Base
+        # from https://chartmogul-samples.s3-eu-west-1.amazonaws.com/public/04_Invoice_line_items.csv
+        HEADERS = %w[Invoice\ external\ ID External\ ID Subscription\ external\ ID Subscription\ set\ external\ ID Type Amount\ in\ cents Plan Service\ period\ start Service\ period\ end Quantity Proration Discount\ code Discount\ amount Tax\ amount Description Transaction\ fee Account\ Code Transaction\ fees\ currency Discount\ description Proration\ type Event\ Order].freeze
+
         writeable_attr :invoice_external_id
         writeable_attr :external_id
         writeable_attr :subscription_external_id
@@ -34,7 +34,7 @@ module ChartMogul
         end
 
         def self.headers
-          ChartMogul::CSV::LineItems::SUBSCRIPTION_HEADERS
+          HEADERS
         end
       end
     end
