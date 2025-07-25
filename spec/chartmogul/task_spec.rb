09d3abbb22e09b0c2625475b3191d4a498ebc117
Stefan Tennigkeit <stefan.tennigkeit@googlemail.com>
2025-07-25 14:38:59 +0200

    Fix task creation error (customer_uuid was omitted from payload) (#169)
    * Fix task creation error (customer_uuid was omitted from payload)
    
    * Update version info
    
    * Revert "Remove integration with code climate"
    
    This reverts commit 9cba5ecface3386eee9b5c05605f3c4c10a0d6e0.
    
    * cleanup after rebase
    
    * fix typo

--- spec/chartmogul/task_spec.rb
+++ spec/chartmogul/task_spec.rb
@@ -23,7 +23,7 @@ describe ChartMogul::Task do
     subject { described_class.new(attrs) }
 
     it 'sets the read-only properties correctly' do
-      expect(subject).to have_attributes({ customer_uuid: nil, task_uuid: nil, created_at: nil, updated_at: nil })
+      expect(subject).to have_attributes({ task_uuid: nil, created_at: nil, updated_at: nil })
     end
 
     it 'sets the writeable properties correctly' do
@@ -38,6 +38,16 @@ describe ChartMogul::Task do
 
     it 'sets all properties correctly' do
       expect(subject).to have_attributes(attrs)
+    end
+  end
+
+  describe '.serialize_for_write' do
+    subject { described_class.new(attrs) }
+
+    it 'serializes the correct payload' do
+      expect(subject.serialize_for_write).to eql(attrs.reject do |k, _|
+        %i[task_uuid created_at updated_at completed_at].include?(k)
+      end)
     end
   end
 
